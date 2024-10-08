ba58e807fff7420cc96e8b8aca5068af2327da79
lbz <labeebbi@buffalo.edu>
2024-10-08 05:51:05 -0400

    Populated Website with data

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         whileInView={{opacity:1, y:0}}
         initial={{opacity:0, y:-100}}
         transition={{duration:0.5}}
-        className='my-10 text-center text-4xl'>Lets get in touch!</motion.h1>
+        className='my-10 text-center text-4xl'>Labeeb.Contact(your_query):</motion.h1>
         <div className='text-center tracking-tighter'>
             <motion.p 
             whileInView={{opacity:1, x:0}}
