44671df40a8c73038f3b7d0963dcefdf33594afe
lbz <labeebbi@buffalo.edu>
2024-10-08 07:19:37 -0400

    changed color of tech text

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -55,7 +55,7 @@ const Experience = () => {
                 {experience.technologies.map((tech, idx) => (
                   <span 
                     key={idx} 
-                    className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>
+                    className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-700'>
                     {tech}
                   </span>
                 ))}
